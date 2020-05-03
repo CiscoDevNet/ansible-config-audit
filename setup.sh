7841740f8a75c63daf336ea85a7bd5bdb4b0a582
securenetwrk <developer@devbox.lab.devnetsandbox.local>
2020-05-03 15:29:01 -0700

    Adding more tasks

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+git clone https://github.com/securenetwrk/gold_config.git
+cd gold_config
 pip install -r requirements.txt
 #ansible-galaxy collection install cisco.ios
 #ansible-galaxy collection install cisco.ios:0.0.3-dev8
