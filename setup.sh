b27ff56cdc862dbf1832918ca97305b719abe0f3
securenetwrk <developer@devbox.lab.devnetsandbox.local>
2020-05-03 13:46:34 -0700

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,4 @@
+pip install -r requirements.txt
+#ansible-galaxy collection install cisco.ios
+#ansible-galaxy collection install cisco.ios:0.0.3-dev8
+ansible-galaxy collection install -r requirements.yml -p ./
