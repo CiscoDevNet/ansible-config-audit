83926ce770491f00a6cea708fc85446d58949aa5
Eric Thiel <erthiel@cisco.com>
2020-05-04 07:29:57 -0700

    Trying roles

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ pip install -r requirements.txt
 #ansible-galaxy collection install cisco.ios
 #ansible-galaxy collection install cisco.ios:0.0.3-dev8
 ansible-galaxy collection install -r requirements.yml -p ./
+
+
+# ansible-playbook 1_auditv2.yml -C -v
