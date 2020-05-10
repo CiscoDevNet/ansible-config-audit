40973e3ceea2e395bb82b7f9cdc4063e5870c047
Eric Thiel <erthiel@cisco.com>
2020-05-10 14:04:38 -0700

    Adding galaxy role

--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@ pip install -r requirements.txt
 #ansible-galaxy collection install cisco.ios
 #ansible-galaxy collection install cisco.ios:0.0.3-dev8
 ansible-galaxy collection install -r requirements.yml -p ./
-
+ansible-galaxy install securenetwrk.gold_config -p roles
 
 # ansible-playbook 1_auditv2.yml -C -v
